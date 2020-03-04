788eedbfc0cfc693fc1defefe23c5ea352ef6fcb
Mohamed Sellu <medesllu2001@gmail.com>
2020-03-04 17:48:34 +0000

    First commit of test codes

---
+++ EVDcode.R
@@ -0,0 +1,6 @@
+## instructions for github packages (latest version)
+
+remotes::install_github("reconhub/epicontacts")
+remotes::install_github("reconhub/linelist")
+remotes::install_github("reconhub/earlyR")
+remotes::install_github("reconhub/projections")
